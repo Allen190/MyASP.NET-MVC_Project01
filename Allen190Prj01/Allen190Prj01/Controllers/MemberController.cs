33725f95a96f6ef740eec9405b917a7def60d2a8
Allen <wei-790105@hotmail.com>
2022-12-28 20:45:16 +0800

    #Allen190-20221228-資料庫用Dapper做紀錄

--- Allen190Prj01/Allen190Prj01/Controllers/MemberController.cs
+++ Allen190Prj01/Allen190Prj01/Controllers/MemberController.cs
@@ -5,26 +5,28 @@ using System.Web;
 using System.Web.Mvc;
 using Allen190Prj01.Models;
 using System.Web.Security;
-
+using System.Data.SqlClient;
+using Dapper;
 
 namespace Allen190Prj01.Controllers
 {
     [Authorize]
     public class MemberController : Controller
     {
-    dbPrj01Entities1 db = new dbPrj01Entities1();
+        dbPrj01Entities1 db = new dbPrj01Entities1();
+        private readonly string ConnStr = "Data Source=.;Initial Catalog=dbPrj01;Integrated Security=True";
         public ActionResult Index()
         {
             DBmanager dBmanager = new DBmanager();
             List<AdoMember> members = dBmanager.GetAdoMembers();
             ViewBag.members = members;
-            return View("../Home/Index","_LayoutMember",members);
+            return View("../Home/Index", "_LayoutMember", members);
         }
 
         public ActionResult Logout()
         {
             FormsAuthentication.SignOut();
-            return RedirectToAction("Login","Home");
+            return RedirectToAction("Login", "Home");
         }
 
         public ActionResult Changestring()
@@ -35,24 +37,47 @@ namespace Allen190Prj01.Controllers
         [HttpPost]
         public ActionResult Changestring(string txt)
         {
-            
-            string [] t= txt.Split(' ');
+
+            string[] t = txt.Split(' ');
             string t0 = t[0];
             string t1 = t[1];
             string t2 = RevereseString(t1);
-            string tt = t0+" "+t1;
-            string ctt = t0+" " +t2;
+            string tt = t0 + " " + t1;
+            string ctt = t0 + " " + t2;
             ViewBag.tt = tt;
             ViewBag.ctt = ctt;
 
-            //tChangeString chnew = new tChangeString();
-            //chnew.fChar = t1;
-            //chnew.fNewChar = t2;
-            //chnew.fUpDateTime=DateTime.Now;
-            //List<tChangeString> list= new List<tChangeString>();
-            //list.Add(chnew);
-            //db.SaveChanges();
-            return View();
+            changestring chnew = new changestring();
+            {
+                chnew.fChar = tt;
+                chnew.fNewChar = ctt;
+                chnew.fUpDateTime = DateTime.Now;
+            }
+           
+
+            string sql = @"Insert into tChangeString
+            (fChar,fNewChar,fUpDateTime)
+            values
+            (@fChar,@fNewChar,@fUpDateTime)";
+
+            using (SqlConnection con = new SqlConnection(ConnStr))
+            {
+                con.Query<changestring>(sql,new{
+                fChar= chnew.fChar,
+                fNewChar= chnew.fNewChar,
+                fUpDateTime= DateTime.Now
+                });
+              
+            }
+
+                return View();
+           
+        }
+        public class changestring
+        {
+            public string fChar;
+            public string fNewChar;
+            public DateTime fUpDateTime;
         }
         public static string RevereseString(string str)
         {
